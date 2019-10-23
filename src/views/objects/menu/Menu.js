28481c14e3994451bc6244fcf7615de453c4f027
Johan Mouchet <mouchet.johan@gmail.com>
2019-10-23 11:35:56 +1100

    Simplified Menu type

--- src/views/objects/menu/Menu.js
+++ src/views/objects/menu/Menu.js
@@ -5,18 +5,18 @@ import cx from "classnames";
 import "./menu.scss";
 import { Button } from "../";
 
-type MenuItem = {
-  text: string,
-  url?: string,
-  button?: Button,
-  metadata?: {
-    additionalClasses?: Array<string>
-  },
-  menu?: [MenuItem]
-};
-
 type Props = {
-  menu: [MenuItem]
+  menu: [
+    {
+      text: string,
+      url?: string,
+      button?: Button,
+      metadata?: {
+        additionalClasses?: Array<string>
+      },
+      menu?: [Props]
+    }
+  ]
 };
 
 const Menu = (props: Props) => {
