04be2431d89155dc341b32b6e8c99e2e5bc91e8a
Johan Mouchet <mouchet.johan@gmail.com>
2020-04-23 17:51:54 +1000

    Added config option to parse()

--- src/utils/parse.js
+++ src/utils/parse.js
@@ -3,6 +3,14 @@
 import DOMPurify from "dompurify";
 import HTMLReactParser from "html-react-parser";
 
-const parse = (html: string) => HTMLReactParser(DOMPurify.sanitize(html));
+const parse = (
+  html: string,
+  HTMLReactParserOptions: ?{ [key: string]: any },
+  DOMPurifyConfig: ?{ [key: string]: any }
+) =>
+  HTMLReactParser(
+    DOMPurify.sanitize(html, DOMPurifyConfig),
+    HTMLReactParserOptions
+  );
 
 export default parse;
