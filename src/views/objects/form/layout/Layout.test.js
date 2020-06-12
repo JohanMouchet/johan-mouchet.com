fc9579c8e19066932cedc26efaa7313e47b19a17
Johan Mouchet <mouchet.johan@gmail.com>
2020-06-12 18:00:29 +1000

    Fixed failed tests

--- src/views/objects/form/layout/Layout.test.js
+++ src/views/objects/form/layout/Layout.test.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Layout from ".";
+import { Fieldset, FormGroup, Label } from ".";
 
-it("Renders <Layout /> without crashing", () => {
-  expect(shallow(<Layout />)).toMatchSnapshot();
+it("Renders <Fieldset /> without crashing", () => {
+  expect(shallow(<Fieldset />)).toMatchSnapshot();
+});
+
+it("Renders <FormGroup /> without crashing", () => {
+  expect(shallow(<FormGroup />)).toMatchSnapshot();
+});
+
+it("Renders <Label /> without crashing", () => {
+  expect(shallow(<Label />)).toMatchSnapshot();
 });
