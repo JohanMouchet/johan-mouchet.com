bdb4d4b75659bad7ee5775bdd968d9bb0629747a
Johan Mouchet <mouchet.johan@gmail.com>
2020-06-12 18:04:37 +1000

    Removed StyleGuide

--- src/views/pages/index.js
+++ src/views/pages/index.js
@@ -1,4 +1,2 @@
 export { default as Home } from "./home";
 export { default as NotFound } from "./not-found";
-// export { default as Storybook } from "./storybook";
-export { default as StyleGuide } from "./styleguide";
