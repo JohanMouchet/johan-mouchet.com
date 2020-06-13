7b96666085037718c0714123cc58af28f66229a5
Johan Mouchet <mouchet.johan@gmail.com>
2020-06-13 16:57:44 +1000

    Added shadows story

---
+++ src/constants/shadows.js
@@ -0,0 +1,7 @@
+import settings from "assets/styles/settings/_settings.scss";
+
+export const SHADOWS = {
+  SM: settings.s_shadows_sm,
+  MD: settings.s_shadows_md,
+  LG: settings.s_shadows_lg,
+};
