0b74891e53df972922dc0e7408124cc5f35e5c8f
Johan Mouchet <mouchet.johan@gmail.com>
2020-06-16 17:33:05 +1000

    Attempt at redirecting Storybook

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <Switch>
     <Route path="/">{Home}</Route>
     <Route path="/storybook">
-      <Redirect to="/storybook/index.html?path=/story/" />
+      <Redirect to="/storybook/index.html" />
     </Route>
     <Route path="/:rest*">{NotFound}</Route>
   </Switch>,
