9e82df0137880594746ecfbed30126cd2255c0e6
Johan Mouchet <mouchet.johan@gmail.com>
2020-06-17 16:21:16 +1000

    Attempt at redirecting Storybook

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Switch, Route, Redirect } from "wouter";
+import { Switch, Route } from "wouter";
 import { Home, NotFound } from "views/pages";
 import * as serviceWorker from "serviceWorker";
 
 ReactDOM.render(
   <Switch>
     <Route path="/">{Home}</Route>
-    <Route path="/storybook/">
-      <Redirect to="/storybook/index.html" />
-    </Route>
     <Route path="/:rest*">{NotFound}</Route>
   </Switch>,
   document.getElementById("root")
