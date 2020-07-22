1cf4c09d91b0f6075e6ac6501b248939cc931dc1
Johan Mouchet <mouchet.johan@gmail.com>
2020-07-22 19:06:22 +1000

    Added Prismic GraphQL

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "assets/styles/_styles.scss";
+// eslint-disable-next-line
+// @ts-ignore
+import { PrismicLink } from "apollo-link-prismic";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Switch, Route } from "wouter";
+import "assets/styles/_styles.scss";
 import { Home, NotFound } from "views/pages";
 import * as serviceWorker from "serviceWorker";
 
+const client = new ApolloClient({
+  link: PrismicLink({
+    uri: "https://johan-mouchet.prismic.io/graphql",
+  }),
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <Switch>
-    <Route path="/">{Home}</Route>
-    <Route path="/:rest*">{NotFound}</Route>
+    <Route path="/">
+      <ApolloProvider client={client}>
+        <Home />
+      </ApolloProvider>
+    </Route>
+    <Route path="/:rest*">
+      <NotFound />
+    </Route>
   </Switch>,
   document.getElementById("root")
 );
