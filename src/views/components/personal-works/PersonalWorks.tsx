548767f4630e5451b28f93e83a510397c0bd28be
Johan Mouchet <mouchet.johan@gmail.com>
2024-06-11 17:02:44 +1000

    Update `<PersonalWorks />` style

--- src/views/components/personal-works/PersonalWorks.tsx
+++ src/views/components/personal-works/PersonalWorks.tsx
@@ -35,39 +35,28 @@ export const PersonalWorks = ({
 
   return (
     <div className={clsx(styles["c-personal-works"], className)} {...props}>
-      <div className="grid">
-        {works.map((work) => (
-          <div
-            className="cell cell-6 sm:cell-4 md:cell-2 u-vr--bottom-1 md:u-vr--bottom-0"
-            key={work.link.url}
-          >
-            <section className={styles["c-personal-work"]}>
-              <a
-                className={styles["c-personal-work__overlay"]}
-                href={work.link.url}
-              >
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  className={styles["c-personal-work__thumbnail"]}
-                  src={work.thumbnailSrc?.url || work.thumbnailExternalSrc?.url}
-                  width={200}
-                  height={200}
-                  alt={`Personal work on ${work.icon}`}
-                  loading="lazy"
-                />
-                {work.icon && (
-                  <span
-                    className={styles["c-personal-work__source"]}
-                    aria-hidden
-                  >
-                    {getIcon(work.icon)}
-                  </span>
-                )}
-              </a>
-            </section>
-          </div>
-        ))}
-      </div>
+      {works.map((work) => (
+        <a
+          className={styles["c-personal-work"]}
+          href={work.link.url}
+          key={work.link.url}
+        >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            className={styles["c-personal-work__thumbnail"]}
+            src={work.thumbnailSrc?.url || work.thumbnailExternalSrc?.url}
+            width={167}
+            height={251}
+            alt={`Personal work on ${work.icon}`}
+            loading="lazy"
+          />
+          {work.icon && (
+            <span className={styles["c-personal-work__source"]} aria-hidden>
+              {getIcon(work.icon)}
+            </span>
+          )}
+        </a>
+      ))}
     </div>
   );
 };
