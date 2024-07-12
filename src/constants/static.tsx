ebb258cf7c1d7815f46492ca35fd115b93769c58
Johan Mouchet <mouchet.johan@gmail.com>
2024-07-12 11:51:13 +1000

    Update tagline

--- src/constants/static.tsx
+++ src/constants/static.tsx
@@ -2,7 +2,7 @@ export const STATIC = {
   profile: {
     firstName: "Johan",
     lastName: "Mouchet",
-    tagline: "Design System Architect",
+    tagline: "Front-End Engineer, Design System Architect",
     location: "Melbourne, Australia",
   },
   version: "7.0.0",
